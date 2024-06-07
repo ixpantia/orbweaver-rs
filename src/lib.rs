0e82c8f41b673268db254edfa247520ef18d0acc
andres <afquinteromoreano@gmail.com>
2024-06-07 14:12:47 -0500

    chore: Adds DirectedGraphBuilder to prelude

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,6 @@ pub mod prelude {
     pub(crate) type GraphInteractionResult<T> = Result<T, GraphInteractionError>;
     pub use crate::directed::acyclic::DirectedAcyclicGraph;
     pub use crate::directed::DirectedGraph;
+    pub use crate::directed::DirectedGraphBuilder;
     pub use crate::error::*;
 }
